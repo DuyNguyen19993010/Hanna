b01626d81f990ece7164e0d76a01ebd652b4c4b1
DuyNguyen19993010 <yuddayama@gmail.com>
2023-10-02 19:54:19 +0100

    modularize front-end

--- backend/database/db.js
+++
@@ -1,9 +0,0 @@
-const {Client} = require('pg');
-const {createClient} = require('@supabase/supabase-js');
-const dotenv = require('dotenv');
-
-dotenv.config({path:__dirname + `/../env/.env.${process.env.NODE_ENV}`}); 
-
-const client = createClient(process.env.API_URL, process.env.API_KEY); 
-
-module.exports = client;
