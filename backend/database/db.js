efcdeb354247021d604070b24021861bc4f77cf4
DuyNguyen19993010 <yuddayama@gmail.com>
2023-10-02 16:37:59 +0100

    added supabase client & change query method to supabase's

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,14 +1,9 @@
 const {Client} = require('pg');
+const {createClient} = require('@supabase/supabase-js');
 const dotenv = require('dotenv');
 
 dotenv.config({path:__dirname + `/../env/.env.${process.env.NODE_ENV}`}); 
 
-const client = new Client({
-    host:process.env.DB_HOST,
-    database:process.env.DB,
-    port:process.env.DB_PORT,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASS
-})
+const client = createClient(process.env.API_URL, process.env.API_KEY); 
 
 module.exports = client;
--- backend/routes/catalog.js
+++ backend/routes/catalog.js
@@ -15,19 +15,6 @@ const fileHandler = require('../fileHandler/utilities/fileHandler');
 // database client
 
 const dbClient = require('../database/db');
-const { join } = require('path');
-
-try{
-
-    dbClient.connect();
-    console.log('Database connection successful');
-
-}
-catch(err){
-
-    console.log('Error database connection');
-
-}
 
 // catalog API:
 
@@ -41,30 +28,21 @@ route.get(get_multi_item_uri, async (req,res)=>{
     try{
 
         // Postgres query
-        
-        let query = await new Promise((resolve,reject)=>{
-            
-            let qstring = 'SELECT * FROM catalog';
-            
-            dbClient.query(qstring,(err,res)=>{
-            
-                if(!err){
 
-                    resolve( res );
+        const {data, err} = await dbClient.
+        from('catalog').select()
+        
+        if(err){
             
-                }
-                
-                else{
+            res.status(400).send(err)
+        
+        }
+        else{
 
-                    reject(res)
-                
-                }            
-            })
-        })
-        
-        // Send item list to client
-        
-        res.send(query.rows);
+            // Send item list to client
+            res.send(data);
+
+        }
 
     }
     catch(err){
@@ -84,39 +62,14 @@ route.get(get_item_uri, async (req,res)=>{
     try{
     
         // Postgres query
-
-        let query = await new Promise((resolve,reject)=>{
-            
-            let qstring = `SELECT * FROM catalog WHERE id='${req.params.item_id}'`;
-            
-            dbClient.query(qstring,(err,res)=>{
-            
-                if(!err){
-            
-                    resolve( res );
-            
-                }
-                
-                else{
-            
-                    reject(res)
-                
-                }            
-            })
-        })
         
-        // Send item list to client
-        
-        if(query.rowCount != 0){
+        const {data, err} = await dbClient.
+        from('catalog').
+        select().
+        eq('name',"bread").
+        single();
 
-            res.send(query.rows);
-
-        }
-        else{
-
-            res.status(410).send(false)
-
-        }
+        res.send(data);
 
     }
     catch(err){
@@ -143,42 +96,21 @@ route.post(post_item_uri, fileHandler.single('img'), async (req,res)=>{
         
         const fpath = path.join(dest,fname);
 
-        let queryRes = await new Promise((resolve, reject) => {
-            
-            let qstring = `INSERT INTO catalog (name,minquan,price,img) 
-                                        VALUES (
-                                        '${req.body.name}',
-                                        ${parseInt(req.body.minQuan)},
-                                        ${parseInt(req.body.price)},
-                                        '${fpath}') 
-                                        RETURNING *`
+        let new_item = {name:req.body.name,minquan:req.body.minQuan,price:req.body.price,img:fpath}
 
-            // let qstring = `INSERT INTO catalog (name,minquan,price) 
-            // VALUES (
-            // '${req.body.name}',
-            // ${parseInt(req.body.minQuan)},
-            // ${parseInt(req.body.price)}) 
-            // RETURNING *`
-
-            dbClient.query(qstring,(err,res)=>{
-            
-                if(!err){
-
-                    resolve(res.rows);
-                
-                }
-                else{
-
-                    reject(err);
-
-                }
-            })
-        })
+        // Insert query
+        
+        const {err} = await dbClient.from('catalog').
+        insert(new_item);
 
         // Send new returned item to the client
         
-        res.send(queryRes);
-    
+        if(!err){
+
+            res.send(new_item);
+
+        }
+        
     }
     catch(err){
 
@@ -195,51 +127,63 @@ const delete_item_uri = '/items/:item_id';
 route.delete(delete_item_uri, async (req,res)=>{
     try{
 
-        let queryRes = await new Promise((resolve, reject)=>{
+        // let queryRes = await new Promise((resolve, reject)=>{
 
-            const qstring = `DELETE FROM catalog WHERE id='${req.params.item_id}'  RETURNING *`
+        //     const qstring = `DELETE FROM catalog WHERE id='${req.params.item_id}'  RETURNING *`
     
-            dbClient.query(qstring,(err,res)=>{
+        //     dbClient.query(qstring,(err,res)=>{
     
-                if(!err){
+        //         if(!err){
     
-                    resolve(res)
+        //             resolve(res)
     
-                }
-                else{
+        //         }
+        //         else{
     
-                    reject(res)
+        //             reject(res)
                 
-                }
-            });
+        //         }
+        //     });
     
-        })
+        // })
 
-        if(queryRes.rowCount != 0){
+        // if(queryRes.rowCount != 0){
 
-            // Delete file from server
+        //     // Delete file from server
             
-            let filePath = queryRes.rows[0].img;
+        //     let filePath = queryRes.rows[0].img;
 
-            try{
+        //     try{
 
-                fs.unlinkSync(filePath);
+        //         fs.unlinkSync(filePath);
 
-            }
-            catch(err){
+        //     }
+        //     catch(err){
 
-                res.status(410).send(false);
+        //         res.status(410).send(false);
 
-            }
-            res.send(queryRes.rows[0]);
+        //     }
+        //     res.send(queryRes.rows[0]);
+
+        // }
+        // else{
+
+        //     res.status(410).send(false);
+
+        // }
+
+        console.log(req.params.item_id);
+        let {err} = await dbClient.from('catalog').delete().eq('id',req.params.item_id);
+        
+        if(!err){
+
+            res.status(200).send(true);
 
         }
         else{
-
-            res.status(410).send(false);
-
+            res.status(400).send(err);
         }
-        
+
     }
     catch(err){
 
